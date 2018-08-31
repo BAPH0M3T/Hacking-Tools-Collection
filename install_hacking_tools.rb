3cf3863f6e8e85c7eabf59b9d8abae6e64f2e387
Jack H. Wharton <38582334+BAPH0M3T@users.noreply.github.com>
2018-09-01 00:08:13 +0100

    Update install_hacking_tools.rb

--- install_hacking_tools.rb
+++ install_hacking_tools.rb
@@ -19,7 +19,7 @@ puts "2..."
 sleep (1)
 puts "1..."
 
-`apt-get install netdiscover macchanger steghide neofetch tshark openvpn`
+`apt-get install netdiscover macchanger steghide neofetch tshark openvpn git`
 `git clone https://github.com/gkbrk/slowloris.git`
 `pip3 install https://download.electrum.org/3.2.2/Electrum-3.2.2.tar.gz`
 `curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall`
